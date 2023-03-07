a10aed03bc3b8d08905e57dfbb3180712c6de20b
Muhammad Ali <791600ali@gmail.com>
2023-03-08 10:34:37 +1100

    home

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,15 @@
 class PlacesController < ApplicationController
+
   def search
+      @places = Place.all
+      if params[:query].present?
+        sql_query = <<~SQL
+          places.name @@ :query
+        SQL
+        @places = Place.where(sql_query, query: "%#{params[:query]}%")
+      else
+        @places = Place.all
+      end
   end
 
   def show
